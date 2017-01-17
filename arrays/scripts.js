a48e02086f77e21b3bb0eae7526d486e11930bd2
Adam Paul <ajp01@acuitysso.com>
2017-01-17 14:00:50 -0500

    Finished second exercise

--- arrays/scripts.js
+++ arrays/scripts.js
@@ -1,5 +1,5 @@
 // Get your shorts on - this is an array workout!
-    // ## Array Cardio Day 1
+    // ## Array Cardio Day 1 and 2
     // Some data we can work with
     const inventors = [
       { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
@@ -16,6 +16,20 @@
       { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
     ];
     const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+
+    const people2 = [
+      { name: 'Wes', year: 1988 },
+      { name: 'Kait', year: 1986 },
+      { name: 'Irv', year: 1970 },
+      { name: 'Lux', year: 2015 }
+    ];
+    const comments = [
+      { text: 'Love this!', id: 523423 },
+      { text: 'Super good', id: 823423 },
+      { text: 'You are the best', id: 2039842 },
+      { text: 'Ramen is my fav food ever', id: 123523 },
+      { text: 'Nice Nice Nice!', id: 542328 }
+    ];
     // Array.prototype.filter()
     // 1. Filter the list of inventors for those who were born in the 1500's
     const filtered = inventors.filter(checkAge);
@@ -157,3 +171,38 @@
     console.log("Reduced es5: ");
     console.log(reducedData);
     console.log("------------------------------------------------");
+
+
+    // Some and Every Checks
+    // Array.prototype.some() // is at least one person 19 or older?
+
+    function isOlderThan19(element, index, array){
+        return element.year < 1999;
+    }
+
+    console.log("9. Is someone in people2 older than 19? ");
+    console.log(people2.some(isOlderThan19));
+    console.log("------------------------------------------------");
+
+    console.log("10. Is everyone in people2 older than 19? ");
+    console.log(people2.every(isOlderThan19));
+    console.log("------------------------------------------------");
+
+    // Find is like filter, but instead returns just the one you are looking for
+    // find the comment with the ID of 823423
+    // Array.prototype.findIndex()
+    // Find the comment with this ID
+    function locateIndex(array) {
+        return array.id === 823423;
+    }
+
+    console.log("11. Find index of 823423 ");
+    console.log(comments.findIndex(locateIndex));
+    console.log("------------------------------------------------");
+
+    // delete the comment with the ID of 823423
+    console.log("12. Delete that index");
+    console.log(comments.splice(comments.findIndex(locateIndex), 1));
+    console.log(comments);
+    console.log("------------------------------------------------");
+    
