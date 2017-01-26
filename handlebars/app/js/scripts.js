4ae3d6556dee129eab7a88c414863369c0144218
Adam Paul <ajp01@acuitysso.com>
2017-01-26 15:18:49 -0500

    Mini handlebars test

--- handlebars/app/js/scripts.js
+++ handlebars/app/js/scripts.js
@@ -1,1 +1,9 @@
-// comments
+document.addEventListener("DOMContentLoaded", function() {
+
+    var source = document.querySelector("#lights-template").innerHTML;
+    template = Handlebars.compile(source);
+
+    var html = template(json);
+    document.querySelector("#data-section").innerHTML = html;
+
+});
